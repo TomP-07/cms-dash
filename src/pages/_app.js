81ca4b753cd15fe8928430be9cce9b577f739f6e
Tomás Ponce Gessi <tomaspgessi@gmail.com>
2022-07-12 16:31:07 -0300

    Initial

---
+++ src/pages/_app.js
@@ -0,0 +1,13 @@
+import PropTypes from 'prop-types';
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+MyApp.propTypes = {
+  Component: PropTypes.any,
+  pageProps: PropTypes.pageProps,
+};
+
+export default MyApp;
